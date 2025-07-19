442bc3881a7735690fd5804c4da4912187069cf4
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2025-07-20 01:09:23 +0600

    Update footer layout and add ThoughtHub branding
    Enhanced the footer design with improved spacing and alignment, updated copyright information, and added a 'Developed & Maintained by ThoughtHub' section with logo. Introduced thoughthub-logo.png image for branding.

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
             <div className='container'>
                 <div className="row">
                     <div className='d-lg-flex justify-content-between'>
-                        <div className='text-center'>
-                            <h4>We are </h4>
-                            <p className="d-md-flex justify-content-evenly">
+                        <div className='text-center mt-3'>
+                            <h5 className='text-md-start my-4'>We are </h5>
+                            <p className="d-md-flex justify-content-between">
                                 <Link to="/about">About Us</Link>
 
                                 <Link to="/contact">Contact</Link>
@@ -18,8 +18,8 @@ const Footer = () => {
                             </p>
                         </div>
 
-                        <div className='text-center'>
-                            <h5>Follow Us</h5>
+                        <div className='text-center mt-3'>
+                            <h5 className='text-md-end my-4'>Follow Us</h5>
                             <div className="d-md-flex justify-content-between">
                                 <Link to="https://www.facebook.com/5pointsAcademy" target='_blank'>
                                     <i className="h3 fab fa-facebook-f"></i>
@@ -34,15 +34,29 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
+            <div className='text-center my-5'>
+                <h4>All Rights Reserved &copy; 2025 5points Academy</h4>
+            </div>
             <hr />
-            <div className='text-center mb-3'>
-                <span>Copyright &copy; 2024
-                    <Link className='text-warning' to="https://ashikurrb.vercel.app" target='_blank'>
-                        @ashikurrb
-                    </Link>
-                    All Rights Reserved
+            <div className="text-center my-2 text-secondary">
+                <span className="text-end">
+                    Developed & Maintained by{' '}
+                    <a
+                        href="https://thoughthub.notion.site"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-flex items-center"
+                    >
+                        <img
+                            src="/images/thoughthub-logo.png"
+                            alt="ThoughtHub Logo"
+                            style={{ height: '24px', marginRight: '5px', verticalAlign: 'middle' }}
+                        />
+                        <strong className='text-secondary'>ThoughtHub</strong>
+                    </a>
                 </span>
             </div>
+
         </div>
     );
 };
